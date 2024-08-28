9e84c9c9eda27f0e7d9caf6dbbd7741928a0a04a
harman <harmandhiman6776@gmail.com>
2024-08-28 11:58:26 +0530

    28/08 - MongoDB, MongoSH, CRUD

---
+++ kuchbhi.js
@@ -0,0 +1,28 @@
+const data = [
+    { "name": "Karan Aujla", "age": 22, "marks": 85 },
+    { "name": "Navaan Sandhu", "age": 24, "marks": 92 },
+    { "name": "Arjan dhillon", "age": 20, "marks": 78 },
+    { "name": "Prem dhillon", "age": 23, "marks": 64 },
+    { "name": "Babbu Maan", "age": 21, "marks": 88 }
+]
+[
+    { "name": "John cena", "age": 22, "subject": "c++", "rating": 4.5 },
+    { "name": "Alice Smith", "age": 24, "subject": "java", "rating": 4.8 },
+    { "name": "Bob Johnson", "age": 20, "subject": "DBMS", "rating": 4.2 },
+    { "name": "Charlie Brown", "age": 23, "subject": "BEE", "rating": 3.9 },
+    { "name": "Diana Prince", "age": 21, "subject": "Python", "rating": 4.7 }
+]
+[
+    { "name": "John cena", "age": 22, "subject": "c++", "rating": 4.5 },
+    { "name": "Alice Smith", "age": 24, "subject": "java", "rating": 4.8 },
+    { "name": "Bob Johnson", "age": 20, "subject": "DBMS", "rating": 4.2 },
+    { "name": "Charlie Brown", "age": 23, "subject": "BEE", "rating": 3.9 },
+    { "name": "Diana Prince", "age": 21, "subject": "Python", "rating": 4.7 }
+]
+[
+    { "name": "Hindveer", "age": 22, "marks": 85 },
+    { "name": "Harmeet", "age": 24, "marks": 92 },
+    { "name": "Honey", "age": 20, "marks": 78 },
+    { "name": "Harman", "age": 23, "marks": 64 },
+    { "name": "spiddy", "age": 21, "marks": 88 }
+]
